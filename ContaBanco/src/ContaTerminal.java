cfbc02867e0eed22807a0f8f20f5886040d7ed53
GabrielaFFS <gabylla8582@gmail.comgit config user.namegit config --global user.email gabylla8582@gmail.com>
2024-05-20 12:24:36 -0300

    Soluções dos desafios

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,35 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class ContaTerminal {
+
+    public static void main(String[] args) {
+
+        double saldo= 2538.50;
+        
+        
+
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        
+        System.out.println("Por favor, digite o número da conta:");
+        int numero = scanner.nextInt();
+
+
+        System.out.println("Por favor, digite o número da agência:");
+         String agencia = scanner.next();
+
+         
+        System.out.println("Por favor, digite o seu nome:");
+        String nomeCliente = scanner.next();
+
+            System.out.println( "Olá" + nomeCliente + " obrigado por criar uma conta em nosso banco. Sua agência é " + agencia + ", conta "  +numero +" e seu saldo " + saldo + " já está disponível para saque.");
+        
+        scanner.close();
+
+    }
+}
+
+    

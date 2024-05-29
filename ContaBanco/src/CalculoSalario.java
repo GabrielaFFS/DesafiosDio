0903d6457ff66bad20134dff5bf1cf67c08dd6b2
GabrielaFFS <gabylla8582@gmail.comgit config user.namegit config --global user.email gabylla8582@gmail.com>
2024-05-28 22:09:58 -0300

    Resolução dos Desafios de código.

---
+++ ContaBanco/src/CalculoSalario.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class CalculoSalario {
+    public static void main(String[] args) {
+        Scanner leitorDeEntradas = new Scanner (System.in);
+
+        System.out.println("Por favor, digite o valor do salário :");
+        float valorSalario = leitorDeEntradas.nextFloat();
+
+
+        System.out.println("Por favor, digite o valor dos benefícios:");
+         float valorBeneficios = leitorDeEntradas.nextFloat();
+
+
+
+        float valorImposto=0;
+        if (valorSalario >= 0 && valorSalario <= 1100){
+            valorImposto = 0.05f * valorSalario;
+        }
+
+        else if (valorSalario >= 1100.01 && valorSalario <= 2500) {
+            valorImposto = 0.10f * valorSalario;
+
+        } else if (valorSalario > 2500) {
+            valorImposto = 0.15f * valorSalario;
+        }
+
+        float saida = valorSalario - valorImposto + valorBeneficios;
+        System.out.println(String.format("%.2f", saida));
+
+        leitorDeEntradas.close();
+    }
+}      

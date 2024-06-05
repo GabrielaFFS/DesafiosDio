f99dea1ca7b56bf91a1d38988c770eb988de5860
GabrielaFFS <gabylla8582@gmail.comgit config user.namegit config --global user.email gabylla8582@gmail.com>
2024-06-05 18:27:28 -0300

    Resolução do desafio.

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -13,10 +13,6 @@ public class Contador {
         
             contar(parametroUm, parametroDois);
             
-            int contagem = parametroDois - parametroUm;
-            for (int i = 1; i <= contagem; i++) {
-                System.out.println("Imprimindo o número " + i);
-            }
             
           
         } catch (Exception e) {
@@ -32,10 +28,14 @@ public class Contador {
         static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
 		 if (parametroUm > parametroDois){
             System.out.println(" O segundo parâmetro deve ser maior que o primeiro");
-        
+         }
+         int contagem = parametroDois - parametroUm;
+         for (int i = 1; i <= contagem; i++) {
+             System.out.println("Imprimindo o número " + i);
+         }
+         
 		
-		
-    }
+    
 }
 }
 

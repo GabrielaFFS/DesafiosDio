079ff8cd6e52a10d1e7ca5b100a4808ef349278b
GabrielaFFS <gabylla8582@gmail.comgit config user.namegit config --global user.email gabylla8582@gmail.com>
2024-07-20 12:30:19 -0300

    Desefio.

---
+++ desafio-dio-api/src/main/Perfumes/me/dio/desafio_dio_api/DesafioDioApiApplication.java
@@ -0,0 +1,13 @@
+package me.dio.desafio_dio_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDioApiApplication.class, args);
+	}
+
+}
---
+++ desafio-dio-api/src/test/java/me/dio/desafio_dio_api/DesafioDioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.desafio_dio_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

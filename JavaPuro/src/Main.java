385df3e9729a96c7e66f43ed1238d66104a4e07b
GabrielaFFS <gabylla8582@gmail.comgit config user.namegit config --global user.email gabylla8582@gmail.com>
2024-07-20 10:36:21 -0300

    Desafio.

--- JavaPuro/src/Main.java
+++ JavaPuro/src/Main.java
@@ -8,21 +8,22 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		
-		
-		SingletonLazy lazy = SingletonLazy.getInstancia();
-		System.out.println(lazy);
-		lazy = SingletonLazy.getInstancia();
-		System.out.println(lazy);
-		
-		SingletonEager eager = SingletonEager.getInstancia();
-		System.out.println(eager);
-		eager = SingletonEager.getInstancia();
-		System.out.println(eager);
-		
-		SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
-		System.out.println(lazyHolder);
-		lazyHolder = SingletonLazyHolder.getInstancia();
-		System.out.println(lazyHolder);
+		SingletonLazy lazy1 = SingletonLazy.getInstancia();
+        SingletonLazy lazy2 = SingletonLazy.getInstancia();
+        System.out.println("SingletonLazy:");
+        System.out.println(lazy1);
+        System.out.println(lazy2);
+
+        SingletonEager eager1 = SingletonEager.getInstancia();
+        SingletonEager eager2 = SingletonEager.getInstancia();
+        System.out.println("SingletonEager:");
+        System.out.println(eager1);
+        System.out.println(eager2);
+
+        SingletonLazyHolder holder1 = SingletonLazyHolder.getInstancia();
+        SingletonLazyHolder holder2 = SingletonLazyHolder.getInstancia();
+        System.out.println("SingletonLazyHolder:");
+        System.out.println(holder1);
+        System.out.println(holder2);
     }
 }
